0434d5e160db723f98fb504274dc4238c181fc0b
Tayyab Ismail <tayyab.ismail22@gmail.com>
2023-04-08 14:13:45 +0500

    fix config

--- DotnetCoreSampleA/Program.cs
+++ DotnetCoreSampleA/Program.cs
@@ -37,6 +37,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("ApplicationSettings"));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
